1cae57e740620bbdcb1b957d5d4925ecb2c05d68
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2024-07-07 22:57:58 +0900

    Update config.py

--- config.py
+++ config.py
@@ -69,13 +69,13 @@ def get_db():
     except exc.DBAPIError as e:
         if e.connection_invalidated:
             print('connection was invalidated')
-    except :
-        connection = pool.acquire()
-        engine = create_engine('oracle+oracledb://',
-                           pool_pre_ping=True,
-                           creator=lambda: connection)
-        SessionLocal = sessionmaker(bind=engine)
-        db = SessionLocal()
+    # except :
+    #     connection = pool.acquire()
+    #     engine = create_engine('oracle+oracledb://',
+    #                        pool_pre_ping=True,
+    #                        creator=lambda: connection)
+    #     SessionLocal = sessionmaker(bind=engine)
+    #     db = SessionLocal()
 
     try:
         yield db
