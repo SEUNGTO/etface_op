89047ea605a22e9cd5880aeb788ece72fb61cd3b
SEUNGTO <tmdduq9008@gmail.com>
2025-06-30 22:26:06 +0900

    영업이익 제외

--- domain/ETF/etf_router.py
+++ domain/ETF/etf_router.py
@@ -216,73 +216,6 @@ def get_code_price_describe(db: Session = Depends(get_db), code: str = ""):
         logger.error(f'[get_code_price_describe]Database operation failed: {e}')
 
 
-
-# def clean_telegram_data(stocks):
-#     data = pd.DataFrame({})
-#     for channel, url in telegramConfig.items():
-#         for stock in stocks:
-#             try:
-#                 msg = telegram_crawller(url, stock)
-#                 msg['시간'] = msg['일자'] + " " + msg['시간']
-#                 msg.drop('일자', axis=1, inplace=True)
-#                 msg['종목명'] = stock
-#                 msg['채널명'] = channel
-#                 msg = msg[['채널명', '종목명', '조회수', '링크', '시간', '메세지']]
-#                 msg.reset_index(drop=True)
-#                 data = pd.concat([data, msg])
-#             except:
-#                 continue
-#     data = data.sort_values('시간', ascending=False)
-#     return data
-
-# def telegram_crawller(url, keyword):
-#     telegram_msgs = {
-#         'msg': []
-#         , 'date': []
-#         , 'time': []
-#         , 'view': []
-#         , 'link': []
-#     }
-#     query = f'{url}?q={keyword}'
-#     response = requests.get(query)
-#     soup = BeautifulSoup(response.content, 'html.parser')
-
-#     for msg in soup.find_all('div', class_='tgme_widget_message_bubble'):
-
-#         msg.find('a').decompose()
-#         _view = msg.find('span', class_='tgme_widget_message_views').text
-
-#         try:
-#             _msg = msg.find('div', class_='tgme_widget_message_text js-message_text').text
-
-#             datetime = pd.to_datetime(msg.find('time', class_='time').attrs['datetime'])
-#             datetime = datetime.tz_convert('Asia/Seoul')
-#             _date = datetime.strftime('%Y-%m-%d')
-#             _time = datetime.strftime('%H:%M')
-
-#             telegram_msgs['msg'].append(_msg)
-#             telegram_msgs['date'].append(_date)
-#             telegram_msgs['time'].append(_time)
-#             telegram_msgs['view'].append(_view)
-
-#         except:
-#             _msg = None
-#             telegram_msgs['msg'].append(_msg)
-#             telegram_msgs['date'].append("-")
-#             telegram_msgs['time'].append("-")
-#             telegram_msgs['view'].append(_view)
-
-#     for uu in soup.find_all('a', class_='tgme_widget_message_date'):
-#         _link = uu.attrs['href']
-#         telegram_msgs['link'].append(_link)
-
-#     telegram_msgs = pd.DataFrame(telegram_msgs)
-#     telegram_msgs.columns = ['메세지', '일자', '시간', '조회수', '링크']
-#     telegram_msgs.sort_values(by=['일자', '시간'], ascending=[False, False], inplace=True)
-
-#     return telegram_msgs.dropna()
-
-
 def standardize_price(data):
     _med = data.median()
     centered = data - _med
@@ -306,7 +239,7 @@ def get_etf_finance(db: Session = Depends(get_db), code: str = ""):
                 WHERE etf_code = '{code}'
                     AND acount_name in 
                         (
-                            '매출액', '영업비용', '영업이익', '법인세차감전 순이익', '당기순이익',
+                            '매출액', '법인세차감전 순이익', '당기순이익',
                             '유동자산', '현금', '매출채권', '재고자산', '비유동자산', '자산총계',
                             '유동부채', '매입채무', '비유동부채', '부채총계', '자본금', '이익잉여금', '자본총계'
                         )
@@ -329,7 +262,7 @@ def get_etf_finance(db: Session = Depends(get_db), code: str = ""):
         data['구분'] = data['계정명'].copy()
         data['구분'] = data['구분'].replace({
             '매출액' : '포괄손익', 
-            '영업이익' : '포괄손익', 
+            # '영업이익' : '포괄손익', 
             '법인세차감전 순이익' : '포괄손익', 
             '당기순이익' : '포괄손익',
             '유동자산' : '자산', 
@@ -349,7 +282,7 @@ def get_etf_finance(db: Session = Depends(get_db), code: str = ""):
         data['순서'] = data['계정명'].copy()
         data['순서'] = data['순서'].replace({
             '매출액' : '01', 
-            '영업이익' : '02', 
+            # '영업이익' : '02', 
             '법인세차감전 순이익' : '03', 
             '당기순이익' : '04',
             '유동자산' : '05', 
