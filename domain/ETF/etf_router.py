3033815c2f74aa4a664cdcae279e60277cade980
SEUNGTO <tmdduq9008@gmail.com>
2025-06-30 22:50:26 +0900

    일부 수정

--- domain/ETF/etf_router.py
+++ domain/ETF/etf_router.py
@@ -329,8 +329,6 @@ def get_ratio(data, code, n_cu) :
         ['재고자산회전일수', 365/(data.loc['매출액', 'amount']/data.loc['재고자산', 'amount'])],
         ['매입채무회전일수', 365/(data.loc['매출액', 'amount']/data.loc['매입채무', 'amount'])],
         
-        ['현금순환주기', 365/(data.loc['매출액', 'amount']/data.loc['매입채무', 'amount'])],
-        
     ]
     ratio = pd.DataFrame(ratio, columns = ['지표명', '값'])
     ratio.loc['현금순환주기', '값'] = ratio.loc['매출채권회전일수', '값'] + ratio.loc['재고자산회전일수', '값'] - ratio.loc['매입채무회전일수', '값']
